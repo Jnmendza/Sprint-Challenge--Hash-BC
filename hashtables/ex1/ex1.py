ad36f6ed554cf7703ab09e1b733a68002b9115ad
Jnmendza <jonatmendoza@gmail.com>
2020-04-10 09:52:14 -0700

    ex1 passes test, mining in the bg

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,6 +13,32 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    # insert the values into your hashtable
+    # loop thru each individual weight inside [weights]
+    for i in range(len(weights)):
+        # use insert function to fill the hashtable with our weights
+        # i is the single value inside the [weights]
+        hash_table_insert(ht, weights[i], i)
+
+    # loop thru the length
+    for i in range(length):
+        """
+        If we store each weight's list index as its value, we can then check 
+        to see if the hash table contains an entry for limit - weight. If it 
+        does, then we've found the two items whose weights sum up to the limit!
+        """
+        value = limit - weights[i]
+        difference = hash_table_retrieve(ht, value)
+
+        """
+        The higher valued index should be placed in the zeroth index and the 
+        smaller index should be placed in the first index.
+        """
+        if difference and difference > i:
+            pair = [difference, i]
+            return pair
+
+    return None
 
     return None
 
